28bb80ad9afa28e91cbd2174d13f86f5a955b181
Jhanavi Bhatt <113221507+jhanavibhatt@users.noreply.github.com>
2025-04-30 18:28:15 +0530

    Update ExampleUnitTest.kt

--- app/src/test/java/com/example/cursoredittextdemo/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cursoredittextdemo/ExampleUnitTest.kt
@@ -15,3 +15,24 @@ class ExampleUnitTest {
         assertEquals(4, 2 + 2)
     }
 }
+const message = ( 
+message: [ 
+token: $(deviceToken.pushToken), 
+notification: { 
+title: getSavedContact ? $getSavedContact.firstName} ${getSavedContact.lastName): formattedNumber, 
+body: receivedData.type == "MMS"? receivedData.media[0].url: isStringNotify (receivedData.text), 
+data: { 
+title: ${formattedNumber), 
+messageType: receivedData.type, 
+body: receivedData.type == "MMS"? receivedData.media[0].url: isStringNotify(receivedData.text), 
+click_action: "https://callweb.io/messages", 
+smallIcon: "https://api.callweb.io/live/images/callweb.png", 
+time: new Date().getTime().toString(), 
+msgBody: receivedData.type === "MMS" ? receivedData.media[0].url: isStringNotify(receivedData.text), 
+number: formattedNumber, 
+displayTime: "Today", 
+senderCount: "e", 
+receiveCount: "0", 
+smsType: "receive", 
+mainSMS: isSubmessageData && isSubmessageData.mainSMSId ? isSubmessageData.mainSMSId: "e", 
+SMSReferenceId: String(pnSMSRefId)
